8dd64ba55668ccd84c4384dec01354de784a3043
Marcelina <marcelina.sitko@interia.pl>
2022-01-13 14:28:33 +0100

    init project

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import './styles/index.scss'
+
+import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap/dist/js/bootstrap.bundle'
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
