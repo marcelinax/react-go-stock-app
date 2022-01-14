92978da8dd415a407603ad7e5f8cbb04ead9a823
Marcelina <marcelina.sitko@interia.pl>
2022-01-14 12:41:14 +0100

    active sidebar link + logo

--- src/components/global/SidebarLink.tsx
+++ src/components/global/SidebarLink.tsx
@@ -1,15 +1,21 @@
+import { Link, } from 'react-router-dom';
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 interface Props {
     icon: React.ReactNode,
-    title: String
+    title: string,
+    to: string
 }
 
-export const SidebarLink: React.FC<Props> = ({icon, title}) => {
+export const SidebarLink: React.FC<Props> = ({ icon, title,to }) => {
+    
+    const location = useLocation();
+
     return (
-        <div className='sidebar-link d-flex flex-column align-items-center p-2 w-100 mb-3 cursor-pointer'>
+        <Link to={to} className={`sidebar-link text-decoration-none d-flex flex-column align-items-center justify-content-center p-2 w-100 mb-3 cursor-pointer position-relative ${to === location.pathname && 'sidebar-link--active'}`}>
             {icon}
-            <p className='text-gray mt-1 text fw-500 fs-6'>{title}</p>
-        </div>
+            <span className={'mt-1 text fw-500 fs-6 text-gray'}>{title}</span>
+        </Link>
     );
 };
---
+++ src/components/global/Table.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Table: React.FC = () => {
+    return (
+        <div />
+    );
+};
