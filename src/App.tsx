16b2a09964e4ec2d14e795e8b1317bfd36b63e67
Marcelina <marcelina.sitko@interia.pl>
2022-01-16 18:20:18 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -12,24 +12,21 @@ import { Homepage } from './views/Homepage';
 import { Products } from './views/Products';
 import { Provider } from 'react-redux';
 import React from 'react';
-import store from './store/store';
 
 function App() {
     return (
-        <Provider store={store}>
-            <BrowserRouter>
-                <ToastContainer position='top-center'/>
-                <Routes>
-                    <Route path='/' element={<Homepage/>} />
-                    <Route path='/products' element={<Products/>} />
-                    <Route path='/new-product' element={<CreateProduct/>} />
-                    <Route path='/new-category' element={<CreateCategory/>} />
-                    <Route path='/categories' element={<Categories/>} />
-                    <Route path='/edit-product/:id' element={<EditProduct/>} />
-                    <Route path='/edit-category/:id' element={<EditCategory/>} />
-                </Routes>
-            </BrowserRouter>
-        </Provider>
+        <BrowserRouter>
+            <ToastContainer position='top-center'/>
+            <Routes>
+                <Route path='/' element={<Homepage/>} />
+                <Route path='/products' element={<Products/>} />
+                <Route path='/new-product' element={<CreateProduct/>} />
+                <Route path='/new-category' element={<CreateCategory/>} />
+                <Route path='/categories' element={<Categories/>} />
+                <Route path='/edit-product/:id' element={<EditProduct/>} />
+                <Route path='/edit-category/:id' element={<EditCategory/>} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
